97a5b301cf858a0d9e3963ea9fe34b19cf417e96
sandeep <sandeepch41@gmail.com>
2024-04-21 11:06:00 -0500

    claude API

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -30,7 +30,7 @@ const Chat = () => {
   };
 
   const fetchOpenAIChatCompletion = async (input) => {
-    const apiKey = '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'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiKey = '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'; // Replace 'YOUR_API_KEY' with your actual API key
     var messageArray = fullInput;
     if(input) {
       const message = {"role": "user", "content": input};
@@ -43,30 +43,34 @@ const Chat = () => {
     const requestOptions = {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${apiKey}`,
+        'x-api-key': 'replace-your-token-here',
+        'Accept': '*/*',
+        'anthropic-version': '2023-06-01',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-          "model": "gpt-3.5-turbo",
-          "messages": messageArray,
-          "temperature": 0.5
+          "model": "claude-3-haiku-20240307",
+          "max_tokens": 2000,
+          "messages": messageArray
       })
     };
   
     try {
-      const response = await fetch('https://api.openai.com/v1/chat/completions', requestOptions);
+      //https://cors-anywhere.herokuapp.com/corsdemo
+      const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.anthropic.com/v1/messages', requestOptions);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
-      const responseMessage = {"role":data.choices[0].message.role, 
-                              "content":data.choices[0].message.content}
+      const responseMessage = {"role": data.role, 
+                              "content": data.content[0].text
+                              }
       //console.log("after response::  " , responseMessage);
       const responseArray = [...messageArray, responseMessage]
       //console.log("after response responseArray::  " , responseArray);
       setFullInput(responseArray);
       //console.log("after response fullInput::  " , fullInput);
-      return data.choices[0].message.content; // Extracting the completed text from the response
+      return data.content[0].text; // Extracting the completed text from the response
     } catch (error) {
       console.error('Error:', error);
       return null;
